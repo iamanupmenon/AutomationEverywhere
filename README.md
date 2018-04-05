a207b9ff8a0b326596455bdf2812d90657a6d119
Anup Menon <anup@moneytap.com>
2018-04-06 02:03:17 +0530

    Automation Everywhere Take 1

---
+++ README.md
@@ -0,0 +1,22 @@
+# automation_everywhere
+
+> This is a new project, Probably will be website that contains my profile, Blog, reviews and maybe hobbies. PS : this is the new begining so remember the date 6th April 2018 1:48AM IST. And WELCOME TO THE WORLD OF POSSIBILITIES. 
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm install # Or yarn install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, checkout the [Nuxt.js docs](https://github.com/nuxt/nuxt.js).
---
+++ nuxt.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'automation_everywhere',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'This is a new project, Probably will be website that contains my profile, Blog, reviews and maybe hobbies. PS : this is the new begining so remember the date 6th April 2018 1:48AM IST. And WELCOME TO THE WORLD OF POSSIBILITIES. ' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
